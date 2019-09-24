f95cf8930c85f629e0c3d19f5f980f6f8dc9c77d
Chris Forbes <christopherforbes1@googlemail.com>
2019-09-24 21:43:16 +0100

    added system salt and user pepper

--- src/main/java/uk/co/cflabs/encryption/Sha512Service.java
+++ src/main/java/uk/co/cflabs/encryption/Sha512Service.java
@@ -5,13 +5,19 @@ import org.springframework.stereotype.Service;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 @Service
 public class Sha512Service
 {
+	private String systemSalt;
+	private final int SALT_LENGTH = 255;
 	public Sha512Service()
 	{
-
+		SecureRandom random = new SecureRandom();
+		byte[] salt = new byte[SALT_LENGTH];
+		random.nextBytes(salt);
+		systemSalt = new String(salt);
 	}
 
 	public String getSHA512SecurePassword(String passwordToHash)
@@ -19,18 +25,24 @@ public class Sha512Service
 		String generatedPassword = null;
 		try
 		{
+			passwordToHash += systemSalt;
+			passwordToHash+=getUserPepper(passwordToHash);
 			MessageDigest md = MessageDigest.getInstance("SHA-512");
 			byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
 			StringBuilder sb = new StringBuilder();
-			for(int i=0; i< bytes.length ;i++)
-			{
-				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			for (byte aByte : bytes) {
+				sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
 			}
 			generatedPassword = sb.toString();
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
 		return generatedPassword;
+	}
+
+	private String getUserPepper(String saltedPassword)
+	{
+		return saltedPassword.substring(0, saltedPassword.length()/2);
 	}
 
 }
